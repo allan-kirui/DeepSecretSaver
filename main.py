8a90d7ae928b48c1974983429ae155f6681274ad
allan-kirui <allan.kipkemoi.kirui@gmail.com>
2024-02-10 13:31:01 +0100

    Added secret management to Account class

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class Account:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.secret = "test"
+        self.secret = "default secret"
 
     def get_username(self):
         return self.username
@@ -17,6 +17,11 @@ class Account:
 
     def get_dark_secret(self):
         return self.secret
+
+    def set_dark_secret(self):
+        print("Please input secret for your account")
+        self.secret = input()
+        print("Secret saved")
 
 class AccountManager:
     def __init__(self):
@@ -70,8 +75,20 @@ if __name__ == '__main__':
 
         valid = accountManager.is_valid_credentials(username, password)
         if valid:
-            secret = accountManager.get_account(username).get_dark_secret()
-            print(accountManager.get_account(username).get_username() + "'s secret is "+ secret)
+            while True:
+                print("To retreive secret please enter 'r'")
+                print("To change secret please enter 'c'")
+                print("To logout please enter 'l'")
+                action = input()
+
+                if action == 'r':
+                    secret = accountManager.get_account(username).get_dark_secret()
+                    print(accountManager.get_account(username).get_username() + "'s secret is " + secret)
+                elif action == 'c':
+                    accountManager.get_account(username).set_dark_secret()
+                elif action == "l":
+                    print("Successfully logged out")
+                    break
         else:
             print("Invalid username or password")
 
